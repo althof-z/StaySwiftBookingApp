25308bbe7c9c53adea6e076c7c86659738863124
I Wayan Ardika Chandra <iwayanchandra2003@gmail.com>
2023-12-05 15:38:55 +0700

    fix login,register and logout

--- app/src/main/java/com/example/hotelbookings/HomeActivity.kt
+++ app/src/main/java/com/example/hotelbookings/HomeActivity.kt
@@ -1,30 +1,48 @@
 package com.example.hotelbookings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        //....
+        // Find the ImageView for the logout button
+        val logoutImageView = findViewById<ImageView>(R.id.logoutImageView)
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.action_home -> {
-                    // Handle click on Home icon
-                    return@setOnNavigationItemSelectedListener true
-                }
-                R.id.action_exit -> {
-                    // Handle click on Exit icon
-                    return@setOnNavigationItemSelectedListener true
-                }
-                else -> false
-            }
+        // Set OnClickListener for the logout ImageView
+        logoutImageView.setOnClickListener {
+            showLogoutConfirmationDialog()
         }
-
     }
+
+    private fun showLogoutConfirmationDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Logout")
+        builder.setMessage("Are you sure you want to logout?")
+        builder.setPositiveButton("Yes") { dialog, which ->
+            logout()
+        }
+        builder.setNegativeButton("No") { dialog, which ->
+            // Do nothing, close the dialog
+        }
+        builder.show()
+    }
+
+    private fun logout() {
+        // Perform logout actions, clear sessions, etc.
+
+        // Start the LoginActivity
+        val intent = Intent(this, LoginActivity::class.java)
+        // Clear the back stack so that the user cannot navigate back to HomeActivity
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
+    }
+
+
 }
