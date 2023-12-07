769fd868bf5311045e593fc0e204818f603817d9
I Wayan Ardika Chandra <iwayanchandra2003@gmail.com>
2023-12-08 00:04:48 +0700

    fix reload from disk

--- app/src/main/java/com/example/hotelbookings/SplashActivity.kt
+++ app/src/main/java/com/example/hotelbookings/SplashActivity.kt
@@ -1,7 +1,6 @@
 package com.example.hotelbookings
 
 import android.content.Intent
-<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -24,46 +23,3 @@ class SplashActivity : AppCompatActivity() {
 
     }
 }
-=======
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.WindowManager
-import android.view.animation.AlphaAnimation
-import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-
-@Suppress("DEPRECATION")
-class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-        val splashImage: ImageView = findViewById(R.id.splash) // replace with your ImageView ID
-
-        // Fade in animation
-        val fadeInAnimation = AlphaAnimation(0.0f, 1.0f)
-        fadeInAnimation.duration = 3000 // Adjust the duration as needed
-
-        splashImage.startAnimation(fadeInAnimation)
-
-        // Handler to start the MainActivity after the animation
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-
-            // Set custom fade-in animation for MainActivity
-            startActivity(intent)
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-
-            finish()
-        }, 3000) // Adjust the delay time to match the animation duration
-    }
-}
->>>>>>> origin/main
