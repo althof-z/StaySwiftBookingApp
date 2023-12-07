5ff95725f7352143cde2e7d9a30d5bb93c8cbf42
AlthofZ <althof.edu@gmail.com>
2023-12-07 10:19:12 +0700

    Change Parcel to Parcelize

--- app/src/main/java/com/example/hotelbookings/data/DataHotel.kt
+++ app/src/main/java/com/example/hotelbookings/data/DataHotel.kt
@@ -1,42 +1,13 @@
 package com.example.hotelbookings.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class DataHotel (
     val name: String,
     val location: String,
     val price: String,
     val description: String, // Tambahkan properti description
     val photo: Int = 0
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readInt()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeString(location)
-        parcel.writeString(price)
-        parcel.writeString(description)
-        parcel.writeInt(photo)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<DataHotel> {
-        override fun createFromParcel(parcel: Parcel): DataHotel {
-            return DataHotel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<DataHotel?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
